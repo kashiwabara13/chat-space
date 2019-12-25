0ed68a9bd36b4f37654b18df99d993e165a260f0
kashiwabara13 <57056793+kashiwabara13@users.noreply.github.com>
2019-12-25 19:40:08 +0900

    Unicorn4

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<13.113.233.209>', user: 'ec2-user', roles: %w{app db web}
+server '13.113.233.209', user: 'ec2-user', roles: %w{app db web}
